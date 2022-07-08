87fa33065db795b0a800b6968b9b09f825058fcb
Otavio Lemos <otaviolemos@gmail.com>
2022-07-08 15:27:52 -0300

    feat: redirector endpoint

--- src/app.py
+++ src/app.py
@@ -1,15 +1,25 @@
-from flask import Flask, request, jsonify
-from factories import make_repo, make_shorten_usecase
+from flask import Flask, request, jsonify, make_response
+from factories import make_repo, make_shorten_usecase, make_redirector_usecase
 
 def create_app():
   app = Flask(__name__)
   repo = make_repo()
+
   @app.route("/shorten", methods = ['POST'])
   def shorten():
     longUrl = request.json["longUrl"]
     shorten_usecase = make_shorten_usecase(repo)
     result = { "hash": shorten_usecase.perform(longUrl) }
     return jsonify(result), 200
+  
+  @app.route("/<hash>", methods = ['GET'])
+  def redirect(hash):
+    redirector_usecase = make_redirector_usecase(repo)
+    longUrl = redirector_usecase.perform(hash)
+    response = make_response()
+    response.headers['location'] = longUrl
+    response.status_code = 301
+    return response
   return app
 
 app = create_app()
--- src/factories.py
+++ src/factories.py
@@ -1,4 +1,5 @@
 from inmemoryurlrepo import InMemoryURLRepo
+from urlredirector import URLRedirector
 from urlshortener import URLShortener
 
 def make_repo():
@@ -6,3 +7,6 @@ def make_repo():
 
 def make_shorten_usecase(repo):
   return URLShortener(repo)
+
+def make_redirector_usecase(repo):
+  return URLRedirector(repo)
