53f93b49169749084761a3bad9cd0168a0c3941a
Otavio Lemos <otaviolemos@gmail.com>
2022-07-12 14:04:45 -0300

    fix: column order

--- src/database/base_script_for_testing.sql
+++ src/database/base_script_for_testing.sql
@@ -2,8 +2,8 @@ create database url_shortener;
 create table url 
 (
 	id int NOT NULL PRIMARY KEY,
-	hash varchar (255) NOT NULL, 
-	longurl varchar (255) NOT NULL
+  longurl varchar (255) NOT NULL,
+	hash varchar (255) NOT NULL
 );
 create user test_user with password '1234';
 grant all privileges on database url to test_user;
