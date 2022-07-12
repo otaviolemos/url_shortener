d614c0867b2f1f57eedad70d4e7aef8197fdfaf4
Otavio Lemos <otaviolemos@gmail.com>
2022-07-12 14:50:12 -0300

    feat: handle app db connect / repo / disconnect

--- src/app.py
+++ src/app.py
@@ -1,19 +1,30 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, g
 from factories import make_repo, make_shorten_usecase, make_redirector_usecase
+import psycopg2
 
 def create_app():
   app = Flask(__name__)
-  repo = make_repo()
+
+  @app.before_request
+  def before_request():
+    host='localhost'
+    database='url_shortener'
+    user='test_user'
+    password='1234'
+    conn = get_db_connection(host, database, user, password)
+    g.db = conn
 
   @app.route("/shorten", methods = ['POST'])
   def shorten():
     longUrl = request.json["longUrl"]
+    repo = make_repo()
     shorten_usecase = make_shorten_usecase(repo)
     result = { "hash": shorten_usecase.perform(longUrl) }
     return jsonify(result), 200
   
   @app.route("/<hash>", methods = ['GET'])
   def redirect(hash):
+    repo = make_repo()
     redirector_usecase = make_redirector_usecase(repo)
     try:
       longUrl = redirector_usecase.perform(hash)
@@ -23,6 +34,17 @@ def create_app():
       return response
     except:
       return "Invalid hash.", 404
+  
+  @app.after_request
+  def after_request(response):
+    if g.db is not None:
+      g.db.close()
+    return response
   return app
+
+
+def get_db_connection(host, database, user, password):
+  conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+  return conn
 
 app = create_app()
