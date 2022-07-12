6790e9be9268644bebbdafc428e442c41008ae99
Otavio Lemos <otaviolemos@gmail.com>
2022-07-12 13:56:27 -0300

    chore: add base db script for testing

---
+++ src/database/base_script_for_testing.sql
@@ -0,0 +1,10 @@
+create database url_shortener;
+create table url 
+(
+	id int NOT NULL PRIMARY KEY,
+	shorturl varchar (255) NOT NULL, 
+	longurl varchar (255) NOT NULL
+);
+create user test_user with password '1234';
+grant all privileges on database url to test_user;
+grant all privileges on table url_shortener.url to test_user;
