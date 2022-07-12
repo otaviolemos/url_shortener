26f9421883b37436dd61661867eee3e4520686e9
Otavio Lemos <otaviolemos@gmail.com>
2022-07-12 14:31:09 -0300

    test: postgres repo adapter

--- tests/adapter.py
+++ tests/adapter.py
@@ -1,11 +1,33 @@
+from postgresurlrepo import PostgresURLRepo
 from urlshortener import generate_id, to_base62
+import psycopg2
 
 def test_postgres_adapter():
-  repo = PostgresURLRepo()
+  host='localhost'
+  database='url_shortener'
+  user='test_user'
+  password='1234'
+  conn = get_db_connection(host, database, user, password)
+  clear_db(conn)
+  repo = PostgresURLRepo(conn)
   id = generate_id()
   longUrl = "https://en.wikipedia.org/wiki/Systems_design"
   hash = to_base62(id)
   repo.add(id, longUrl, hash)
   assert len(repo.list()) == 1
-  assert repo.findByLongUrl() == hash
+  assert repo.findByLongUrl(longUrl) == hash
   assert repo.getLongUrl(id) == longUrl
+  clear_db(conn)
+  close_connection(conn)
+
+def get_db_connection(host, database, user, password):
+  conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+  return conn
+
+def close_connection(conn):
+  conn.close()
+
+def clear_db(conn):
+  cur = conn.cursor()
+  cur.execute('DELETE FROM url;')
+  conn.commit()
