222c071b8199e553ca888b4e98df1e6eae5ebb7d
Otavio Lemos <otaviolemos@gmail.com>
2022-07-12 14:50:31 -0300

    feat: use postgres adapter

--- src/factories.py
+++ src/factories.py
@@ -1,9 +1,11 @@
 from inmemoryurlrepo import InMemoryURLRepo
+from postgresurlrepo import PostgresURLRepo
 from urlredirector import URLRedirector
 from urlshortener import URLShortener
+from flask import g
 
 def make_repo():
-  return InMemoryURLRepo()
+  return PostgresURLRepo(g.db)
 
 def make_shorten_usecase(repo):
   return URLShortener(repo)
